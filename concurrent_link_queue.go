9ba92647e0ca58ef10f088a9876dab48e7ca602b
Stone-afk <1711865140@qq.com>
2023-04-21 23:21:07 +0800

    feature: add queue

--- concurrent_link_queue.go
+++ concurrent_link_queue.go
@@ -1,1 +1,41 @@
 package concurrent_queue
+
+import (
+	"sync/atomic"
+	"unsafe"
+)
+
+type ConcurrentLinkBlockingQueue[T any] struct {
+	head  unsafe.Pointer
+	tail  unsafe.Pointer
+	count uint64
+}
+
+func NewConcurrentLinkedQueue[T any]() *ConcurrentLinkBlockingQueue[T] {
+	head := &node[T]{}
+	ptr := unsafe.Pointer(head)
+	return &ConcurrentLinkBlockingQueue[T]{
+		head: ptr,
+		tail: ptr,
+	}
+}
+
+func (c *ConcurrentLinkBlockingQueue[T]) IsFull() bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (c *ConcurrentLinkBlockingQueue[T]) IsEmpty() bool {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (c *ConcurrentLinkBlockingQueue[T]) Len() uint64 {
+	// 在你读的过程中，就被人改了
+	return atomic.LoadUint64(&c.count)
+}
+
+type node[T any] struct {
+	next unsafe.Pointer
+	val  T
+}
