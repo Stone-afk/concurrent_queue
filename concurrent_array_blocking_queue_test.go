ca5f6aa61a4e05bfec13c9fe07ab68884032ebf5
stonehuang <stonehuang@canway.net>
2023-04-21 13:47:13 +0800

    feature add op test

--- concurrent_array_blocking_queue_test.go
+++ concurrent_array_blocking_queue_test.go
@@ -2,14 +2,50 @@ package concurrent_queue
 
 import (
 	"context"
+	"math/rand"
 	"sync"
 	"testing"
+	"time"
 )
 
-// 切片实现
-// BenchmarkConcurrentQueue-12       100000               306.7 ns/op           223 B/op          4 allocs/op
+func TestConcurrentBlockingQueue(t *testing.T) {
+	// 只能确保没有死锁
+	q := NewConcurrentArrayBlockingQueueV2[int](10000)
+	// data := make(chan int, 10000000000000000000000)
+	// 并发的问题都落在 m 上
+	// var m sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(30)
+	for i := 0; i < 20; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				// 你没有办法校验这里面的中间结果
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				val := rand.Int()
+				_ = q.Enqueue(ctx, val)
+				cancel()
+			}
+			wg.Done()
+		}()
+	}
 
-// BenchmarkConcurrentQueue-12       100000               280.8 ns/op           208 B/op          4 allocs/op
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				_, _ = q.Dequeue(ctx)
+				cancel()
+			}
+			wg.Done()
+		}()
+	}
+
+	// 怎么校验 q 对还是不对
+	wg.Wait()
+}
+
+// 切片实现
+// BenchmarkConcurrentQueue-8       2783775               413.3 ns/op
 func BenchmarkConcurrentQueue(b *testing.B) {
 	var wg sync.WaitGroup
 	q := NewConcurrentArrayBlockingQueueV2[int](100)
