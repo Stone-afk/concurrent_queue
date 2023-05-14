4d6919a8c28083ce2ccd1c6691c11a6c52e25453
stonehuang <stonehuang@canway.net>
2023-05-14 12:17:47 +0800

    add DelayQueue

--- delay_queue.go
+++ delay_queue.go
@@ -1,1 +1,15 @@
 package concurrent_queue
+
+import "sync"
+
+type DelayQueue[T Delayable] struct {
+	pq            *PriorityQueue[T]
+	mu            sync.RWMutex
+	dequeueSignal *cond
+	enqueueSignal *cond
+}
+
+type cond struct {
+	signal chan struct{}
+	l      sync.Locker
+}
