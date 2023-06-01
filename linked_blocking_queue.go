52be7ff43eecad0e8d51da947bbe7544c31cdaf0
stonehuang <stonehuang@canway.net>
2023-06-01 16:42:21 +0800

    add LinkedBlockingQueue

---
+++ linked_blocking_queue.go
@@ -0,0 +1,8 @@
+package concurrent_queue
+
+type LinkedBlockingQueue[T any] struct{}
+
+func NewConcurrentLinkedBlockingQueue[T any](capacity int) *LinkedBlockingQueue[T] {
+	// TODO implement me
+	panic("implement me")
+}
