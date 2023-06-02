6083bacbf568cd452ecb894fbe507e4910543f35
stonehuang <stonehuang@canway.net>
2023-06-02 11:38:15 +0800

    add LinkedBlockingQueue

--- linked_blocking_queue.go
+++ linked_blocking_queue.go
@@ -1,8 +1,15 @@
 package concurrent_queue
 
+import "context"
+
 type LinkedBlockingQueue[T any] struct{}
 
 func NewLinkedBlockingQueue[T any](capacity int) *LinkedBlockingQueue[T] {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (q *LinkedBlockingQueue[T]) Enqueue(ctx context.Context, data T) error {
 	// TODO implement me
 	panic("implement me")
 }
