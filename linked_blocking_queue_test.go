1946bed732d481dd44f8387de2c727bea6ea73da
stonehuang <stonehuang@canway.net>
2023-06-28 18:42:46 +0800

    feature: add Dequeue

--- linked_blocking_queue_test.go
+++ linked_blocking_queue_test.go
@@ -3,8 +3,42 @@ package concurrent_queue
 import (
 	"context"
 	"fmt"
+	"github.com/stretchr/testify/require"
+	"math/rand"
+	"sync"
+	"testing"
 	"time"
 )
+
+func TestLinkedBlockingQueue(t *testing.T) {
+	// 并发测试，只是测试有没有死锁之类的问题
+	// 先进先出这个特性依赖于其它单元测试
+	// 也依赖于代码审查
+	q := NewLinkedBlockingQueue[int](100)
+	var wg sync.WaitGroup
+	wg.Add(1000)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			val := rand.Int()
+			err := q.Enqueue(ctx, val)
+			cancel()
+			require.NoError(t, err)
+		}()
+	}
+	go func() {
+		for i := 0; i < 1000; i++ {
+			go func() {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				_, err := q.Dequeue(ctx)
+				cancel()
+				require.NoError(t, err)
+				wg.Done()
+			}()
+		}
+	}()
+	wg.Wait()
+}
 
 func ExampleNewLinkedBlockingQueue() {
 	// 创建一个容量为 10 的有界并发阻塞队列，如果传入 0 或者负数，那么创建的是无界并发阻塞队列
